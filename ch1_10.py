b216ed2dc7707d134a27e2015554c5264b9b0bcc
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2017-11-13 12:03:03 -0500

    v1
    _

--- ch1_10.py
+++ ch1_10.py
@@ -1,3 +1,12 @@
 # Name
-# Problem #
+# Problem #10
 # Chapter 1 Problem Set Part 1
+from time import sleep
+ans = 7 ** 4
+print("As I was going to St. Ives, I met a man with seven wives. Every wife had seven sacks, and every sack had seven cats, and every cat had seven kittens.  Kittens, cats, sacks, and wives, how many were going to St. Ives?    There are interesting aspects to this puzzle, such as who is actually going to St. Ives. For our purposes assume that everyone and everything is headed to St. Ives.  Lets find out.")
+sleep(3)
+print("A man had seven wives. That would be seven one time, or 7^1")
+sleep(3)
+print("Each wife had seven sacks. That would be seven seven times, or 7^7")
+sleep(3)
+print("Each wife had seven sacks. That would be ")
