156e9dfc7f0328a2fc2b5adc9c065f0ff93cfc00
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2017-11-13 11:44:46 -0500

    Finished Version
    _

--- ch1_09.py
+++ ch1_09.py
@@ -1,3 +1,23 @@
 # Name
-# Problem #
+# Problem #9
 # Chapter 1 Problem Set Part 1
+from time import sleep
+num = int(input("Enter a number:"))
+
+print("We are now adding 2 to your number.")
+sleep(3)
+num += 2
+print("We are now multiplying your number by 3.")
+sleep(3)
+num *= 3
+print("We are now subtracting 6 from your number.")
+sleep(3)
+num -= 6
+print("We are now dividing your number by 3.")
+sleep(3)
+num /= 3
+print("Here is your number:")
+sleep(5)
+print(num)
+sleep(10)
+
