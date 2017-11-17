831b184febfc0660b6804bedf3102da2dbebe015
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2017-11-17 15:22:32 -0500

    Final Version

--- ch1_10.py
+++ ch1_10.py
@@ -3,10 +3,24 @@
 # Chapter 1 Problem Set Part 1
 from time import sleep
 ans = 7 ** 4
-print("As I was going to St. Ives, I met a man with seven wives. Every wife had seven sacks, and every sack had seven cats, and every cat had seven kittens.  Kittens, cats, sacks, and wives, how many were going to St. Ives?    There are interesting aspects to this puzzle, such as who is actually going to St. Ives. For our purposes assume that everyone and everything is headed to St. Ives.  Lets find out.")
-sleep(3)
-print("A man had seven wives. That would be seven one time, or 7^1")
-sleep(3)
-print("Each wife had seven sacks. That would be seven seven times, or 7^7")
-sleep(3)
-print("Each wife had seven sacks. That would be ")
+
+print("As I was going to St. Ives, I met a man with seven wives. Every wife had seven sacks, and every sack had seven cats, and every cat had seven kittens.  Kittens, cats, sacks, and wives, how many were going to St. Ives?    There are interesting aspects to this puzzle, such as who is actually going to St. Ives. For our purposes assume that everyone and everything is headed to St. Ives.  Lets find out who's headed to St. Ives.")
+sleep(10)
+
+print("A man had seven wives. That would be seven one time, or 7^1.")
+sleep(2)
+print(7**1)
+print("Every wife had seven sacks. That would be seven two times, or 7^2.")
+sleep(2)
+print(7**2)
+print("Every sack had seven cats. That would be seven three times, or 7^3.")
+sleep(2)
+print(7**3)
+print("Every cat had seven kittens. That would be seven four, or 7^4")
+sleep(2)
+print(7**4)
+print("Kittens, cats, sacks, and wives, how many were going to St. Ives?")
+sleep(2)
+print(7**4)
+sleep(1)
+print("The man and I were also going to St. Ives. Therefore a total of", 7**4+2,"things were going to St. Ives.")
