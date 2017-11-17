3c7bdd888731b796c24309abf5f8336cc3dec4c3
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2017-11-17 15:57:35 -0500

    Final Version

--- ch1_17.py
+++ ch1_17.py
@@ -1,3 +1,9 @@
 # Name
-# Problem #
+# Problem #17
 # Chapter 1 Problem Set Part 1
+#You can print a letter or special character as a string, but not as an integer or a float-point value.  You can print a number as a string, integer, or float-point value.
+var = input("Type something here:")
+print(str(var))
+print(float(var))
+print(int(var))
+
