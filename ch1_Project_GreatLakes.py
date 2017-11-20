ba76f0ea6ab223718970931eac69b408121efc96
MiaKPup <32400855+MiaKPup@users.noreply.github.com>
2017-11-20 11:28:28 -0500

    Final Version

--- ch1_Project_GreatLakes.py
+++ ch1_Project_GreatLakes.py
@@ -1,3 +1,7 @@
 # Name
 # Project: Great Lakes are how big?
 # Chapter 1 Problem Set Part 1
+print("The area of 48 contiguous states is 8,080,464.3 km2.")
+print("The volume of the Great Lakes is 22,810 km3.")
+
+print("The depth of all the water in the Great Lakes when spread evenly across the 48 contiguous U.S. states is",(22810/8080464.3)*1000,"meters.")
